9534bc83fbcae4ed727703a4f74698e3edf185ae
Mir Arif Hasan <arif.ishan05@gmail.com>
2024-01-04 19:00:37 +0600

    fix: code scanning issue

--- packages/hoppscotch-backend/src/utils.ts
+++ packages/hoppscotch-backend/src/utils.ts
@@ -179,7 +179,7 @@ export const validateSMTPUrl = (url: string) => {
  * @returns boolean
  */
 export const validateUrl = (url: string) => {
-  const urlRegex = /^(https?):\/\/(-\.)?([^\s\/?\.#-]+\.?)+(\/[^\s]*)?$/i;
+  const urlRegex = /^(https?):\/\/(-\.)?([^\s\/?\.#-]+\.?)+(\/[^\s]*)?$/;
   return urlRegex.test(url);
 };
 
